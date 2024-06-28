496435d196c991243c6986610740ba8b7d359097
yawnnn <alessandromy7476@gmail.com>
2024-06-29 01:02:25 +0200

    linked list

---
+++ src/linkedlist.c
@@ -0,0 +1,170 @@
+#include <stdlib.h>
+#include "linkedlist.h"
+
+static LLNode *LLNode_new(void *data) {
+    LLNode *node;
+
+    node = malloc(sizeof(LLNode));
+    node->data = data;
+    node->next = NULL;
+
+    return node;
+}
+
+static inline void LLNode_free(LLNode *node, Func_Free func_free) {
+    if (func_free)
+        func_free(node->data);
+    free(node);
+}
+
+void LinkedList_init(LinkedList *list) {
+    list->head = NULL;
+    list->tail = NULL;
+}
+
+void LinkedList_free(LinkedList *list, Func_Free func_free) {
+    if (!LinkedList_is_empty(list)) {
+        LLNode *curr, *next;
+
+        curr = list->head;
+        next = curr->next;
+
+        while (next) {
+            curr = next;
+            next = next->next;
+            LLNode_free(curr, func_free);
+        }
+
+        LLNode_free(list->head, func_free);
+        list->head = list->tail = NULL;
+    }
+}
+
+LLNode *LinkedList_next(LinkedList *list, LLNode *curr) {
+    if (curr)
+        return curr->next;
+    else
+        return list->head;
+}
+
+LLNode *LinkedList_next_back(LinkedList *list, LLNode *curr) {
+    if (curr) {
+        LLNode *prev = NULL;
+
+        while ((prev = LinkedList_next(list, prev)) != NULL && prev->next != curr)
+            ;
+        return prev;
+    } else
+        return list->tail;
+}
+
+LLNode *LinkedList_push_back(LinkedList *list, void *data) {
+    LLNode *node;
+
+    node = LLNode_new(data);
+
+    if (!LinkedList_is_empty(list)) {
+        list->tail->next = node;
+        list->tail = node;
+    } else
+        list->head = list->tail = node;
+
+    return node;
+}
+
+LLNode *LinkedList_push_front(LinkedList *list, void *data) {
+    LLNode *node;
+
+    node = LLNode_new(data);
+
+    if (!LinkedList_is_empty(list)) {
+        node->next = list->head;
+        list->head = node;
+    } else
+        list->head = list->tail = node;
+
+    return node;
+}
+
+LLNode *LinkedList_insert(LinkedList *list, LLNode *prev, void *data) {
+    if (prev) {
+        LLNode *node;
+
+        node = LLNode_new(data);
+        node->next = prev->next;
+        prev->next = node;
+
+        return node;
+    } else
+        return LinkedList_push_front(list, data);
+}
+
+void *LinkedList_pop_back(LinkedList *list) {
+    if (!LinkedList_is_empty(list)) {
+        LLNode *to_remove;
+        void *data;
+
+        to_remove = list->tail;
+        data = to_remove->data;
+
+        if (list->head == list->tail)
+            list->head = list->tail = NULL;
+        else {
+            LLNode *prev;
+
+            if ((prev = LinkedList_next_back(list, list->tail)) != NULL) {
+                prev->next = NULL;
+                list->tail = prev;
+            }
+        }
+
+        LLNode_free(to_remove, NULL);
+
+        return data;
+    }
+
+    return NULL;
+}
+
+void *LinkedList_pop_front(LinkedList *list) {
+    if (!LinkedList_is_empty(list)) {
+        LLNode *to_remove;
+        void *data;
+
+        to_remove = list->head;
+        data = to_remove->data;
+
+        if (list->head == list->tail)
+            list->head = list->tail = NULL;
+        else
+            list->head = list->head->next;
+
+        LLNode_free(to_remove, NULL);
+
+        return data;
+    }
+
+    return NULL;
+}
+
+void *LinkedList_remove(LinkedList *list, LLNode *node) {
+    if (!LinkedList_is_empty(list) && node) {
+        LLNode *prev;
+        void *data;
+
+        if (list->head == node) {
+            list->head = node->next;
+            data = node->data;
+            LLNode_free(node, NULL);
+        } else if ((prev = LinkedList_next_back(list, node)) != NULL) {
+            prev->next = node->next;
+            data = node->data;
+            LLNode_free(node, NULL);
+        } else
+            data = NULL;
+
+        return data;
+    }
+
+    return NULL;
+}
---
+++ src/linkedlist.h
@@ -0,0 +1,134 @@
+/**
+ * @file linkedlist.h
+ */
+
+#ifndef __LINKEDLIST_H__
+#define __LINKEDLIST_H__
+
+#include <stdbool.h>
+
+/**
+ * @brief linked list's node
+ */
+typedef struct LLNode {
+    void *data;             /**< the node's data */
+    struct LLNode *next;    /**< the next node */
+} LLNode;
+
+/**
+ * @brief linked list
+ */
+typedef struct LinkedList {
+    LLNode *head;     /**< beginning of the list */
+    LLNode *tail;     /**< end of the list */
+} LinkedList;
+
+/**
+ * @brief callback to free nodes
+ */
+typedef void (*Func_Free)(void *);
+
+/**
+ * @brief initialize the list
+ *
+ * @param list linked list
+ */
+void LinkedList_init(LinkedList *list);
+
+/**
+ * @brief free the list
+ * 
+ * won't free the nodes's data, only the list, if @p func_free is NULL
+ * 
+ * @param list linked list
+ * @param func_free callback to free the nodes's data
+ */
+void LinkedList_free(LinkedList *list, Func_Free func_free);
+
+/**
+ * @brief return the node after @p curr
+ *
+ * if @p curr == NULL, returns the first node
+ * 
+ * @param list linked list
+ * @param curr current node
+ * @return the next node
+ */
+LLNode *LinkedList_next(LinkedList *list, LLNode *curr);
+
+/**
+ * @brief return the node before @p curr
+ *
+ * if @p curr == NULL, returns the last node
+ * 
+ * @param list linked list
+ * @param curr current node
+ * @return the next node
+ */
+LLNode *LinkedList_next_back(LinkedList *list, LLNode *curr);
+
+/**
+ * @brief insert node at the end of the list
+ *
+ * @param list linked list
+ * @param data node's data
+ * @return the node inserted
+ */
+LLNode *LinkedList_push_back(LinkedList *list, void *data);
+
+/**
+ * @brief insert node at the beginning of the list
+ *
+ * @param list linked list
+ * @param data node's data
+ * @return the node inserted
+ */
+LLNode *LinkedList_push_front(LinkedList *list, void *data);
+
+/**
+ * @brief insert node after @p prev
+ * 
+ * if @p prev is NULL, insert at the beginning of the list
+ *
+ * @param list linked list
+ * @param prev node before the one to be inserted
+ * @param data node's data
+ * @return the node inserted
+ */
+LLNode *LinkedList_insert(LinkedList *list, LLNode *prev, void *data);
+
+/**
+ * @brief remove node from the end of the list
+ *
+ * @param list linked list
+ * @return the node's data
+ */
+void *LinkedList_pop_back(LinkedList *list);
+
+/**
+ * @brief remove node from the beginning of the list
+ *
+ * @param list linked list
+ * @return the node's data
+ */
+void *LinkedList_pop_front(LinkedList *list);
+
+/**
+ * @brief remove @p node from the list
+ *
+ * @param list linked list
+ * @return the node's data
+ */
+void *LinkedList_remove(LinkedList *list, LLNode *node);
+
+/**
+ * @brief if the list contains data
+ *
+ * @param list linked list
+ * @return boolean
+ */
+inline bool LinkedList_is_empty(LinkedList *list) {
+    return list->head == (void *)0;
+}
+
+#endif /* __LINKEDLIST_H__ */
